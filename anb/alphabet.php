4577f91496249a2fcfbc81409b4908e79b6e41f4
Jhojans <jhojansmosquerac@gmail.com>
2021-08-03 19:48:10 -0500

    abc styles 2.0

--- anb/alphabet.php
+++ anb/alphabet.php
@@ -24,50 +24,56 @@
     include_once '../config/connect.php';
 
     ?>
+    <h1>Primero lo primero... ¿Cómo suenan las letras?</h1>
 
     <div class="container">
         <div class="notes">
             <div class="tag">
+                <h3>Recuerda</h3>
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est earum culpa accusamus natus alias, 
                 nihil doloremque vero sed cupiditate tempore praesentium nisi. Natus perferendis omnis ipsum eveniet ut quia recusandae?
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis fuga est maxime id temporibus quasi 
-                ratione maiores dolorem, voluptatem sit vitae iste quas libero neque? Molestiae consequatur omnis doloribus id?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             </div>
             <div class="tag">
+                <h3>Por lo mismo...</h3>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste excepturi consequatur cumque facilis ut consectetur, 
                 qui animi nihil quod voluptas ducimus itaque eos non sed perspiciatis, amet culpa dicta adipisci.
             </div>
         </div>
-        <table class="default" cellspacing=0>
-           <thead>
-               <tr>
-                   <th>Español</th>
-                   <th>Inglés</th>
-                   <th>Pronunciación Inglés</th>
-                   <th>Audio</th>
-               </tr>
-           </thead>
-           <tbody>
-               <?php 
-                    $search = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1006205761.Higlot") or die("Otra f");
-                    $result = "SELECT id, letter_es, letter_en, pronunciation FROM abc";
-    
-                    $resu = pg_query($search, $result);
-    
-    
-                        
-                        while($row = pg_fetch_assoc($resu)){
-                            echo 
-                            '<tr class="tr">
-                                <td>'.$row['letter_es'].'</td>
-                                <td>'.$row['letter_en'].'</td>
-                                <td>'.$row['pronunciation'].'</td>
-                                <td></td>
-                            </tr>';
-                        }
-                ?>
-           </tbody>
-        </table>
+        <div class="table_out">
+            <table class="default" cellspacing=0>
+               <thead>
+                   <tr>
+                       <th>Español</th>
+                       <th>Inglés</th>
+                       <th>Pronunciación Inglés</th>
+                       <th>Audio</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   <?php 
+                        $search = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=1006205761.Higlot") or die("Otra f");
+                        $result = "SELECT id, letter_es, letter_en, pronunciation FROM abc";
+        
+                        $resu = pg_query($search, $result);
+        
+        
+                            
+                            while($row = pg_fetch_assoc($resu)){
+                                echo 
+                                '<tr class="tr">
+                                    <td>'.$row['letter_es'].'</td>
+                                    <td>'.$row['letter_en'].'</td>
+                                    <td>'.$row['pronunciation'].'</td>
+                                    <td></td>
+                                </tr>';
+                            }
+                    ?>
+               </tbody>
+            </table>
+            <button>Continuar</button>
+        </div>
+
     </div>
 
 
